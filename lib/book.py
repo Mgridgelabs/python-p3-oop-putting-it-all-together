3cc5e05784da7962850472ce974b1947cb607e67
Mgridgelabs <mainaianndungu@gmail.com>
2024-09-01 19:32:34 +0300

    completes oop

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,43 @@
 #!/usr/bin/env python3
 
+from turtle import title
+
+
+
 class Book:
-    pass
-    
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def title(self):
+        """The title of the book"""
+        return self._title
+
+    @title.setter
+    def title(self, new_title):
+        """Sets the title of the book"""
+        if isinstance(new_title, str):
+            self._title = new_title
+        else:
+            raise ValueError("Title must be a string.")
         
+    @property
+    def page_count(self):
+        """The number of pages in the book"""
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, count):
+        if isinstance(count, int):
+            self._page_count = count
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        """Simulates turning a page in the book"""
+        print("Flipping the page...wow, you read fast!")
+
+book = Book("And Then There Were None",272)
+print(book.title)
+print(book.page_count)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "Used"  # Default condition when a shoe is created
+
+    @property
+    def size(self):
+        """The size of the shoe"""
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        """Sets the size of the shoe"""
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+    
+    def cobble(self):
+        """Repairs the shoe and sets its condition to 'New'"""
+        print("Your shoe is as good as new!")
+        self.condition = "New"
